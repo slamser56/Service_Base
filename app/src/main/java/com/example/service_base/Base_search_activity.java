28e810694a603d75f6b160a34709af05eaf77f6e
Valentin <slamser56@gmail.com>
2019-09-04 21:14:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/service_base/Base_search_activity.java
@@ -0,0 +1,14 @@
+package com.example.service_base;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Base_search_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_search_activity);
+    }
+}
